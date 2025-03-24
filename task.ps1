18375a0746c147fade1871f4dde888cc7568be03
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-03-24 23:33:27 +0100

    fix var  name

--- task.ps1
+++ task.ps1
@@ -3,13 +3,10 @@ $resourceGroupName = "mate-azure-task-15"
 
 $virtualNetworkName = "todoapp"
 $vnetAddressPrefix = "10.20.30.0/24"
-
 $webSubnetName = "webservers"
 $webSubnetIpRange = "10.20.30.0/26"
-
 $dbSubnetName = "database"
 $dbSubnetIpRange = "10.20.30.64/26"
-
 $mngSubnetName = "management"
 $mngSubnetIpRange = "10.20.30.128/26"
 
@@ -19,11 +16,11 @@ New-AzResourceGroup -Name $resourceGroupName -Location $location
 Write-Host "Creating a virtual network ..."
 $webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
 $dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubne = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
+$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
 
 New-AzVirtualNetwork `
   -Name $virtualNetworkName `
   -ResourceGroupName $resourceGroupName `
   -Location $location `
   -AddressPrefix $vnetAddressPrefix `
-  -Subnet $webSubnet, $dbSubnet, $mngSubne
+  -Subnet $webSubnet, $dbSubnet, $mngSubnet
